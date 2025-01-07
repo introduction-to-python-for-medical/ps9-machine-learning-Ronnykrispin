15fbaf4f222f71b85da2a59abb207345d5754c03
Ronnykrispin <ronnykrispin@gmail.com>
2025-01-07 19:32:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import pandas as pd
-df = pd.read_csv("parkinsons.csv") 
-df= df.dropna()
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from joblib import dump
 
-features = ['PPE' , 'DFA']
+df = pd.read_csv("parkinsons.csv")
+df = df.dropna()
+
+features = ['MDVP:Fo(Hz)', 'MDVP:Jitter(%)']  # מאפיינים שנבחרו
 target = "status"
 X = df[features]
 y = df[target]
 
-from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 X_scaled = scaler.fit_transform(X)
 
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
-from sklearn.ensemble import RandomForestClassifier
-model = RandomForestClassifier(random_state=42)
+model = RandomForestClassifier(n_estimators=200, max_depth=10, random_state=42)  # פרמטרים לשיפור דיוק
 model.fit(X_train, y_train)
 
-from sklearn.metrics import accuracy_score
+
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
+print(f"Model accuracy: {accuracy}")
 
-from joblib import dump
+
 dump(model, "model.joblib")
