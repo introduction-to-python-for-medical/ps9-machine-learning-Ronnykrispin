6af575d9fd462725431602d9d985f60118c178d7
Ronnykrispin <ronnykrispin@gmail.com>
2025-01-07 19:10:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import pandas as pd
+df = pd.read_csv("parkinsons.csv")
+df= df.dropna()
 
+features = ['PPE' , 'DFA']
+target = "status"
+X = df[features]
+y = df[target]
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+X_scaled = scaler.fit_transform(X)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+from sklearn.metrics import accuracy_score
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
+
+dump(model, "model.joblib")
