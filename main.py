9ef8b6e568ac1b6333ad52d362527b4bda31d1ab
Ronnykrispin <ronnykrispin@gmail.com>
2025-01-08 19:15:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-from joblib import dump
+from joblib 
 
 # קריאת הנתונים
 df = pd.read_csv("parkinsons.csv")
@@ -23,14 +23,14 @@ X_scaled = scaler.fit_transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
 # אימון המודל
-model = SVC()
-model.fit(X_train, y_train)
+knn=KNeighborsClassifier(n_neighbors=10)
+knn.fit(X_train, y_train)
 
 # בדיקת המודל
-y_pred = model.predict(X_test)
+y_pred = knn.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
 
 # שמירת המודל
-dump(model, "Ronny_model.joblib")
+joblib.dump(knn, "Ronny_model.joblib")
 print("Model saved successfully!")
